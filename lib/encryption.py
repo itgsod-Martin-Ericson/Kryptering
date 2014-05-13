a38af901618abe2f23ab47b41d43600475e13569
itgsod-Martin-Ericson <Martin.Ericson@elev.it-gymnasiet.se>
2014-05-13 09:27:32 +0200

    Finished the next to subtestets

--- lib/encryption.py
+++ lib/encryption.py
@@ -1,3 +1,6 @@
-def encrypt(string, banan):
-    if len(string) == 0:
+def encrypt(cleartext, offset):
+    if len(cleartext) == 0:
         raise ValueError('can not encrypt empty string')
+    if offset == 0:
+        raise ValueError('offset must not be zero')
+    return "lawl"
