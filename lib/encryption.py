466632855dc740f0e5352bc032d978b87bb17180
itgsod-Martin-Ericson <Martin.Ericson@elev.it-gymnasiet.se>
2014-05-20 10:40:09 +0200

    added variables and main function of the function
    Sorry for not commiting the changes more frequently

--- lib/encryption.py
+++ lib/encryption.py
@@ -1,6 +1,19 @@
 def encrypt(cleartext, offset):
+    alphabet = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
+    crypto_string = ""
+    cleartextupper = cleartext.upper()
     if len(cleartext) == 0:
         raise ValueError('can not encrypt empty string')
     if offset == 0:
         raise ValueError('offset must not be zero')
-    return "lawl"
+    for character in cleartextupper:
+        if character == " ":
+            crypto_string = crypto_string + " "
+        else:
+            clear_index = alphabet.index(character)
+            crypto_index = clear_index + offset
+            while crypto_index >= 26:
+                crypto_index = crypto_index - 26
+            crypto_letter = alphabet[crypto_index]
+            crypto_string = crypto_string + crypto_letter
+    return(crypto_string)
